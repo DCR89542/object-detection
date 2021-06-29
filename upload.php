721349d14c2ff226d36c907d972dfb08ba6f36d1
vero <42807466+SIR-vero@users.noreply.github.com>
2021-06-29 18:16:54 +0530

    taking file as input and storing locally

--- upload.php
+++ upload.php
@@ -4,6 +4,7 @@ if(isset($_POST['detect']) && $_POST['detect']=="Detect")
 	$dir='uploads/';
 	$image_path=$dir.basename($_FILES['imageInput']['name']);
 
+
 	if(move_uploaded_file($_FILES['imageInput']['tmp_name'],$image_path))
 	{
 		echo 'uploaded succesfully.';
