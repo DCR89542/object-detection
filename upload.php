a1ab01867c99a86643964f63bce340647d04131d
vero <42807466+SIR-vero@users.noreply.github.com>
2021-06-30 10:13:54 +0530

    backend working, result stored in ./outputs

--- upload.php
+++ upload.php
@@ -8,6 +8,14 @@ if(isset($_POST['detect']) && $_POST['detect']=="Detect")
 	if(move_uploaded_file($_FILES['imageInput']['tmp_name'],$image_path))
 	{
 		echo 'uploaded succesfully.';
+		$command = "cd backend/ && python yolo.py ".$_FILES['imageInput']['name'];
+		$last_line = system($command, $retval);
+		if($retval == 0){
+			header("Location: viewResult.php");
+		}
+		else {
+			echo "Error Occurred";
+		}
 	}
 }
 ?>
